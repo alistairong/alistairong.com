97d7d3033e59f10115cc8c1bdabdf887b5d7d7eb
alistairong <ongalistair@gmail.com>
2019-07-10 21:51:02 +0800

    Updates

---
+++ precache-manifest.1296c0cb72a4728e7f2718407013b9c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee623dc69e38c7a7e9c8d55a2f7a5d41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49f7709034b3215f830",
+    "url": "/static/css/main.555565dc.chunk.css"
+  },
+  {
+    "revision": "c9dceb7bed28d10c0e7f",
+    "url": "/static/js/2.e48d3ca9.chunk.js"
+  },
+  {
+    "revision": "c49f7709034b3215f830",
+    "url": "/static/js/main.771ece71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
