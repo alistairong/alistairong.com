33fcf9489fe1197cbfb634393322d449a9da2325
alistairong <ongalistair@gmail.com>
2019-07-10 21:34:57 +0800

    initial commit

---
+++ precache-manifest.5d66950429d0c20bd52c9e553f0db67a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adba62906c0c8fc71cc395e862fa81e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07a7d9b4bd9d5126c4eb",
+    "url": "/static/css/main.555565dc.chunk.css"
+  },
+  {
+    "revision": "c9dceb7bed28d10c0e7f",
+    "url": "/static/js/2.e48d3ca9.chunk.js"
+  },
+  {
+    "revision": "07a7d9b4bd9d5126c4eb",
+    "url": "/static/js/main.df2ef5eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
